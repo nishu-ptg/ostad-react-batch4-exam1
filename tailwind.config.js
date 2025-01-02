ee14ce4c09ac6648f00caa6291aea00825d3bd57
Nishu <nishuptg@yahoo.com>
2025-01-02 20:58:07 +0600

    install work sans from google fonts as closest match for figma hire go template

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Work Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
